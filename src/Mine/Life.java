b65b5a3c30a585cad22e2e631d6e7e2639ed35be
KrissAsterisk <crisrogamer@gmail.com>
2026-02-04 13:10:40 +0200

    cleaned up main with a switch expression, check new TODOs for next iteration

--- src/Mine/Life.java
+++ src/Mine/Life.java
@@ -6,7 +6,7 @@ import java.util.*;
 import static Mine.Colours.AnsiCodes.*;
 
 import static java.lang.System.*;
-import static java.lang.Thread.sleep;
+//import static java.lang.Thread.sleep;
 
 
 public class Life {
@@ -23,6 +23,7 @@ public class Life {
         return pName;
     }
 
+    private static final int NAME_CHANGE = 7, FIGHT = 1, SLEEP = 2, DRINK = 3, EAT = 4, CONDITION = 5, QUIT = 6;
 
 
 
@@ -31,7 +32,6 @@ public class Life {
         Colours.clear(); // initialize enum
         //MADD Colours = new MADD();
         int j = 0;
-        int k = 0;
         do{
             try{
                     int l = 50;
@@ -47,81 +47,78 @@ public class Life {
                         out.println("\nIf you would like to change your name, type 7.\nIf not, enter a number, in order to continue.");
                         Scanner tempScanner = new Scanner(in);
                         int tempInt = tempScanner.nextInt();
-                        if (tempInt == 7) {
+                        if (tempInt == NAME_CHANGE) {
                             pName = actions.pNameChange();
                             player1.pName = pName;
                         }
                         actions.showChoices(l, 0);
                         actions.getpName(pName);
-                              for (int i = 1; i <= l; i++) {
-                        int x = reader.nextInt();
-                        if (x > 6 || x < 1) {
-                            out.println("Choose a number between 0 and 7. You lose a move every time you type otherwise.");
-                        } else if (x == 1) {
-                            //l += player1.fight();
-                            l += actions.fightTEST();
-                            if (l < 0) {
-                                continue;
-
-                            }
-                        } else if (x == 2) {
-                            //player1.sleep();
-                           checkInt = actions.sleepTEST();
-                            if (checkInt < 0){
-                                l = checkInt;
-                                continue;
-                            }
-                        } else if (x == 3) {
-                            //player1.drinkWater();
-                            checkInt = actions.drinkWaterTEST();
-                            if (checkInt < 0){
-                                l = checkInt;
-                                continue;
-                            }
-                        } else if (x == 4) {
-                            //player1.eatFood();
-                            checkInt = actions.eatFoodTEST();
-                            if (checkInt < 0){
-                                l = checkInt;
-                                continue;
-                            }
-                        } else if (x == 5) {
-                            //player1.statusReport(); //TODO this is - switch maybe?
-                            actions.statusReportTEST();
-                        } else {
-                            int result = actions.endGameTEST();
-                            if (result == 1) {
-                                actions.showChoices(l, i);
-                                l+=1;
-                                continue;
-                            } else {
-                                out.println("In total you've had " + temp + " moves!");
-                                HighScores highScores = new HighScores();
-                                highScores.filePrintHS(player1, temp);
-                                System.exit(0);
+                        for (int i = 1; i <= l; i++) {
+                            int x = reader.nextInt();
+                            if (x > 6 || x < 1) {
+                                out.println("Choose a number between 0 and 7. You lose a move every time you type otherwise.");
+                                i--;
+                                l--; // don't count for final score.
+                            } else switch (x){
+                                case FIGHT -> {
+                                    l+=actions.fightTEST();
+                                    if(l < 0){ continue; }
                                 }
-                        }
-                        //out.printf("\n1 - Fight\n2 - Sleep\n3 - Drink\n4 - Eat\n5 - Condition\n6 - Quit\n%d: Moves left\n", l - i);
-                        actions.showChoices(l, i);
-                        if (l - i == 5) {
-
-                            out.printf("%s%s+----------------------+%s\n%s%s|      %s %s%sWARNING!%s %s      |%s\n%s%s|You have 5 moves left!|%s\n%s%s+----------------------+%s%n", Colours.ANSI_BLACK_BACKGROUND, ANSI_YELLOW.colourCode(), ANSI_RESET.colourCode(), Colours.ANSI_BLACK_BACKGROUND, ANSI_YELLOW.colourCode(), Colours.ANSI_RED_BACKGROUND, Colours.ANSI_BLACK_BACKGROUND, ANSI_YELLOW.colourCode(), Colours.ANSI_RED_BACKGROUND, Colours.ANSI_BLACK_BACKGROUND, ANSI_RESET.colourCode(), Colours.ANSI_BLACK_BACKGROUND, ANSI_YELLOW.colourCode(), ANSI_RESET.colourCode(), Colours.ANSI_BLACK_BACKGROUND, ANSI_YELLOW.colourCode(), ANSI_RESET.colourCode());
-
-                            /*try {
-                                sleep(2000);
-                                for (int s = 0; s < 25; s++) {
-                                    madd.rngCConst();
-                                    sleep(100);
+                                case SLEEP -> {
+                                    if ((checkInt = actions.sleepTEST()) < 0) {
+                                        l = checkInt; // runs status check which on top of giving current stats, check if you're dead TODO: run the "death check" on a separate thread at all times
+                                        continue;
+                                    }
                                 }
-                                actions.showChoices(l, i);
-                            } catch (InterruptedException e) {
-                                out.println("PATIENCE!");
-                            } //TODO turn on for a little bit of fun ;) */
-                        }
-                        temp = i - 1;
-                        temp ++;
+                                case DRINK -> {
+                                    if((checkInt = actions.drinkWaterTEST()) < 0) {
+                                        l = checkInt;
+                                        continue;
+                                    }
+                                }
+                                case EAT -> {
+                                    if( (checkInt = actions.eatFoodTEST()) < 0){
+                                        l = checkInt;
+                                        continue;
+                                    }
+                                }
+                                case CONDITION -> {
+                                    actions.statusReportTEST();
+                                    i--;
+                                    l--; // don't count towards the final score but still uses a move
+                                }
+                                case QUIT -> {
+                                    if( (actions.endGameTEST()) == 1){
+                                        i--;
+                                        actions.showChoices(l, i);
+                                        continue;
+                                    } else {
+                                        out.println("In total you've had " + temp + " moves!");
+                                        HighScores highScores = new HighScores();
+                                        highScores.filePrintHS(player1, temp);
+                                        System.exit(0);
+                                    }
+                                }
+                            }
+                            actions.showChoices(l, i);
+                            if (l - i == 5) {
 
-                    }
+                                out.printf("%s%s+----------------------+%s\n%s%s|      %s %s%sWARNING!%s %s      |%s\n%s%s|You have 5 moves left!|%s\n%s%s+----------------------+%s%n", Colours.ANSI_BLACK_BACKGROUND, ANSI_YELLOW.colourCode(), ANSI_RESET.colourCode(), Colours.ANSI_BLACK_BACKGROUND, ANSI_YELLOW.colourCode(), Colours.ANSI_RED_BACKGROUND, Colours.ANSI_BLACK_BACKGROUND, ANSI_YELLOW.colourCode(), Colours.ANSI_RED_BACKGROUND, Colours.ANSI_BLACK_BACKGROUND, ANSI_RESET.colourCode(), Colours.ANSI_BLACK_BACKGROUND, ANSI_YELLOW.colourCode(), ANSI_RESET.colourCode(), Colours.ANSI_BLACK_BACKGROUND, ANSI_YELLOW.colourCode(), ANSI_RESET.colourCode());
+
+                                /*try {
+                                    sleep(2000);
+                                    for (int s = 0; s < 25; s++) {
+                                        madd.rngCConst();
+                                        sleep(100);
+                                    }
+                                    actions.showChoices(l, i);
+                                } catch (InterruptedException e) {
+                                    out.println("PATIENCE!");
+                                } //TODO turn on for a little bit of fun ;) */
+                            }
+                            temp = i;
+                        }
+                    temp++; // count the last move before death
                     ANSI_HIGH_INTENSITY.printCode();
                     ANSI_RED.printCode();
                     out.println("You died! Game over!");
@@ -132,7 +129,7 @@ public class Life {
                     tempScanner.close();
                     reader.close();
                     System.exit(0);
-                } catch (InputMismatchException e) {
+                } catch (InputMismatchException e) { //TODO:doesnt work as intended
                 ANSI_RED.printCode();
                 if (j <= 5) {
                     out.println("Please input a number and nothing else.");
